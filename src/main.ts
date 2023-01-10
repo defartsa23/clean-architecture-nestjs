23b11875cece62493053ea133897dbc8a2b098d7
Deza Farras Tsany <deza.ftsany@gmail.com>
2023-01-10 18:35:13 +0800

    update maain

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,33 @@
+import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { FastifyAdapter, NestFastifyApplication } from '@nestjs/platform-fastify';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const logger = new Logger('NestApplication');
+  
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter()
+  );
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+    }),
+  );
+  
+  const config = new DocumentBuilder()
+    .setTitle('Bank Scrap')
+    .setDescription('Bank scrap automation.')
+    .setVersion('2.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('swagger', app, document);
+  
+  const port = 3000
+  await app.listen(port);
+  logger.log(`Starting Nest application ON port ${port}`);
 }
 bootstrap();
