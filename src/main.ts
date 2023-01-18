e25365ebe152e18c4f92f903669c7ef499dd885b
Deza Farras Tsany <deza.ftsany@gmail.com>
2023-01-18 21:00:07 +0800

    config

--- src/main.ts
+++ src/main.ts
@@ -3,17 +3,18 @@ import { NestFactory } from '@nestjs/core';
 import { FastifyAdapter, NestFastifyApplication } from '@nestjs/platform-fastify';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
-import { environmentConfigService } from './config/config.service';
 import { NodeEnvironmentEnum } from './delivery/enums/nodeEnvironment.enum';
 
 async function bootstrap() {
   const logger = new Logger('NestApplication');
+
+  const env = process.env.NODE_ENV;
   
   const app = await NestFactory.create<NestFastifyApplication>(
     AppModule,
     new FastifyAdapter(),
     {
-      logger: (environmentConfigService.getNodeEnv() === NodeEnvironmentEnum.Production) ? ['log', 'error', 'warn', 'verbose'] : ['log', 'error', 'warn', 'debug', 'verbose'] 
+      logger: (env === NodeEnvironmentEnum.Production) ? ['log', 'error', 'warn', 'verbose'] : ['log', 'error', 'warn', 'debug', 'verbose'] 
     }
   );
 
@@ -27,10 +28,11 @@ async function bootstrap() {
     .setDescription('Description.')
     .setVersion('1.0.0')
     .build();
+
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('swagger', app, document);
   
-  const port = environmentConfigService.getNodePort();
+  const port = parseInt(process.env.NODE_PORT) | 3000;
   await app.listen(port, '0.0.0.0');
   logger.log(`Starting Nest application ON port ${port}`);
 }
